0c2f618c909c14a51f591976b3320df4a2aa037f
WParedes2007 <wparedes-2020379@kinal.edu.gt>
2024-08-19 11:13:19 -0600

    Juegos Finalizado

--- src/main/java/com/grupo5/webapp/steam/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo5.webapp.steam.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grupo5.webapp.steam.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-
-}
